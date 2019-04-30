7acd2a3bf9ecac10fca444d072045f4ce98a0886
unknown <blings96@mail.fresnostate.edu>
2019-04-29 17:34:21 -0700

    Final push for test2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,10 +8,11 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
+  data: any = {};
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-
+    this.data = JSON.parse(localStorage.getItem('calculatedData'));
   }
 
 }
